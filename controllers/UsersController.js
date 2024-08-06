2fdf022040d8d952f65ae2050b66b67095aec920
Sibongile <sibongile.nhlema@gmail.com>
2024-08-06 23:27:33 +0200

    resolved eslint errors

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -2,6 +2,7 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class UsersController {
+  // GET /me
   static async getMe(req, res) {
     const token = req.headers['x-token'];
     if (!token) {
@@ -9,21 +10,21 @@ class UsersController {
     }
 
     const key = `auth_${token}`;
-    try {
-      const userId = await redisClient.get(key);
-      if (!userId) {
-        return res.status(401).json({ error: 'Unauthorized' });
-      }
+    const userId = await redisClient.get(key);
 
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    try {
       const user = await dbClient.db.collection('users').findOne({ _id: userId });
       if (!user) {
-        return res.status(401).json({ error: 'Unauthorized' });
+        return res.status(404).json({ error: 'User not found' });
       }
-
-      res.status(200).json({ id: user._id, email: user.email });
+      return res.status(200).json(user);
     } catch (err) {
-      console.error('Error retrieving user:', err);
-      res.status(500).json({ error: 'Internal server error' });
+      console.error('Error fetching user:', err);
+      return res.status(500).json({ error: 'Internal server error' });
     }
   }
 }
